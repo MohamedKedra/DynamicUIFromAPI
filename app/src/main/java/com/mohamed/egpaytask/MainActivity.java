630aa0053d7ba8af76eba851691c81d1c7d52c45
Mohamed Ali Kedra <mohamed.a.kedra@gmail.com>
2020-01-22 23:26:59 +0200

    update design and display data

--- app/src/main/java/com/mohamed/egpaytask/MainActivity.java
+++ app/src/main/java/com/mohamed/egpaytask/MainActivity.java
@@ -3,7 +3,13 @@ package com.mohamed.egpaytask;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import java.util.List;
 
@@ -12,13 +18,51 @@ public class MainActivity extends AppCompatActivity implements MainView {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        MainPresenter presenter = new MainPresenter(this,this);
+        MainPresenter presenter = new MainPresenter(this, this);
         presenter.getAllModels();
     }
 
     @Override
     public void updateUI(List<DataModel> dataModels) {
-        Log.d("res : ",dataModels.size()+"");
+        Log.d("res : ", dataModels.size() + "");
+        LinearLayout layout = linearContainer();
+        for (int i = 0; i < dataModels.size(); i++) {
+            DataModel model = dataModels.get(i);
+            if (model.type.equals("string")) {
+                EditText editText = new EditText(this);
+                editText.setHint(model.name);
+                editText.setInputType(InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
+                editText.setId(model.id);
+                layout.addView(editText);
+            } else if (model.type.equals("number")) {
+                EditText editText = new EditText(this);
+                editText.setHint(model.name);
+                editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                editText.setId(model.id);
+                layout.addView(editText);
+            } else if (model.type.equals("date")) {
+
+            } else if (model.type.equals("textarea")) {
+
+            } else if (model.type.equals("select")) {
+
+            }
+        }
+        Button button = new Button(this);
+        button.setText("Submit");
+        layout.addView(button);
+        setContentView(layout);
+    }
+
+    private LinearLayout linearContainer() {
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        layout.setLayoutParams(params);
+        return layout;
+    }
+
+    enum Type {
+        string, number, date, textarea, select
     }
 }
